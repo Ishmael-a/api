369d413d56a35814da3ca2a06f7b714a193bed26
Ishmael <abuishmaelyusif204@gmail.com>
2024-08-07 14:08:22 -1200

    trying to debug put and delete cart functionality

--- jiji/views.py
+++ jiji/views.py
@@ -21,3 +21,19 @@ class ProductViewSet(viewsets.ModelViewSet):
 class CartViewSet(viewsets.ModelViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
+
+    def update(self, request, *args, **kwargs):
+        # This will handle the PUT request
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        
+        # return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        # This will handle the DELETE request
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        # return Response(status=status.HTTP_204_NO_CONTENT)
